22869c6b981522c8d4da4d9dc79a0caf235909c7
Anton Voronin <voronin13011990@gmail.com>
2024-06-26 09:15:40 +0300

    Commit_2

--- script.js
+++ script.js
@@ -1,39 +1,67 @@
-const wrap   = document.querySelector('.wrapper'),
-      squere = document.querySelectorAll('.squere');
+window.onload = function() {
 
-startGame(500);
-//можно динамически задавать число квадратов, время и генерить квадраты скриптом через innerHTML например
-//а вёрстка будет чистая
+    //можно динамически задавать число квадратов, время и генерить квадраты скриптом через innerHTML например
+    //а вёрстка будет чистая ?
+    const gField = document.querySelector('.gameField'),
+    squere     = document.querySelectorAll('.squere'),
+    score      = document.querySelector('.score'),
+    btnPlay    = document.querySelector('.play');
 
-function startGame(time) {
-    const start = new Date();
-    const timer = setInterval( active, time ); 
+    let start;
 
-    squere.forEach( el=> {
-        el.addEventListener('click', function() {
-            if( this.classList.contains('active') ) {
-                this.classList.add('hide');
-                let hideNum = document.querySelectorAll('.hide');
-                if( hideNum.length === 9 ) {
-                    alert(`Game Over!!! Ваше время ${ ((new Date() - start)/1000).toFixed(2) } сек`);
-                    clearInterval(timer);
+    startGame(1000);
+    
+    function startGame(time) {
+        //восстанавливавем квадратики при следующем запуске:
+        squere.forEach( el=> {
+            el.classList.remove('hide');
+        } );
+
+        start = new Date();
+        let hideNum = 0;
+
+        const timer = setInterval(active, time);
+        //const timer = setTimeout( function() { setTimeout(active, time) } , time ); 
+
+        squere.forEach( el=> {
+            el.addEventListener('click', function(event) {
+                if( event.target.classList.contains('active') ) {
+                    event.target.classList.add('hide');
+                    hideNum++ ;
+
+                    if( hideNum === 9 ) {
+                        let timeGame = ( (new Date() - start)/1000 ).toFixed(1) ;
+                        score.innerHTML += `Ваше время: ${ timeGame }сек <br>`;
+                        clearInterval(timer);
+
+                        if( confirm("Сыграем ещё?") )  startGame(time);
+                    }
+                    // let hideNum = document.querySelectorAll('.hide');
+                    // if( hideNum.length === 3 ) {
+                    // .....
+                    // }
                 }
-            }
-        })
-    } )
-}
+            })
+        } )
+    }
 
-function active() {
-    squere.forEach( el=> {
-        if( getRand() ) el.classList.add('active');
-                   else el.classList.remove('active');
-    } )
-}
+    //btnPlay.addEventListener('click', function() { startGame(1000) } );
 
-function getRand() {
-    return Math.round(Math.random());
+    function active() {
+        squere.forEach( el=> {
+            if( getRand() ) el.classList.add('active');
+                        else el.classList.remove('active');
+        } )
+    }
+
+    function getRand() {
+        return Math.round(Math.random());
+    }
+    // рандомн целое от 0 до max-1
+    // function getRandomInt(max) {
+    //     return Math.floor(Math.random() * max);
+    // }
 }
-// рандомн целое от 0 до max-1
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-  }
+    
+
+
